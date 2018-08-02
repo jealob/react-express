dee14949e68a0d6a41e0e8715d898bc33e57f020
jealob <jealob@gmail.com>
2018-08-02 11:01:14 -0500

    Add MVC files

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Render
-app.get("/api/sharks", function (req, res) {
-  console.log('hit /api/sharks');
-  res.json([
-    { type: "hello world"}
-  ]);
-  res.end();
-});
+// Add routes, both API and view
+app.use(routes);
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-app.listen(PORT, function() {
+// // Render
+// app.get("/api/sharks", function (req, res) {
+//   console.log('hit /api/sharks');
+//   res.json([
+//     { type: "hello world" }
+//   ]);
+//   res.end();
+// });
+
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
